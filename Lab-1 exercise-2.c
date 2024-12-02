c24ed5384d1e64c2466d854da6895c12ab26c4bc
Thamuditha Veenath <159011911+VeenathT@users.noreply.github.com>
2024-12-02 10:46:09 +0530

    Add files via upload

---
+++ Lab-1 exercise-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{ 
+    /* Display "my first C program"
+	the words are separated by tabs */	
+	
+	printf("my\tfirst\tC\tprogram\n");
+	
+    return 0;
+	 
+} //end of function main
+
