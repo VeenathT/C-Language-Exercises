c4f8a74a3ed10b2dafef6285418a13dc8047de13
Thamuditha Veenath <159011911+VeenathT@users.noreply.github.com>
2024-12-02 10:48:15 +0530

    Add files via upload

---
+++ Lab-2 exercise-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+	int no1,no2,sum;
+	
+	printf("Enter two integers:");
+	scanf("%d%d",&no1,&no2);
+	
+	sum=no1+no2;
+	printf("The summation is: %d",sum);
+	
+	return 0;
+	
+}
