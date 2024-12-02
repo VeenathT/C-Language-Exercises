ac2bd3f3da56b404c191028f43016644ceb60a82
Thamuditha Veenath <159011911+VeenathT@users.noreply.github.com>
2024-12-02 10:44:55 +0530

    Add files via upload

---
+++ Lab-1 exercise-1.c
@@ -0,0 +1,12 @@
+//This program display the text "hello world"
+
+#include<stdio.h>
+
+//function main begins program execution
+int main(void)
+{
+	printf("Hello world \n");
+	
+	return 0;
+	
+}//end of function main
