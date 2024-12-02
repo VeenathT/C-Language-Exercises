432a866c0352a56be39ecf8c99b113dce93a4e47
Thamuditha Veenath <159011911+VeenathT@users.noreply.github.com>
2024-12-02 10:48:47 +0530

    Add files via upload

---
+++ Lab-3 exercise-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+	float r, h, areaRec, areaCircle, area; 
+	 
+	printf("Enter radius of the Cylinder : "); 
+	scanf("%f", &r);
+	
+	printf("Enter height of the Cylinder : "); 
+	scanf("%f", &h); 
+	 
+	areaRec = 2 * 22.0/ 7 * r * h;
+	areaCircle = 2*22.0/ 7 * r * r; 
+	area = areaRec + areaCircle; 
+	
+	printf("Surface area of cylinder : %.1f\n", area);
+	 
+	return 0;
+}
+
