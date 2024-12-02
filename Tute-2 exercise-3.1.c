b1905caf8597e82fa236b7337df09f016b494fe5
Thamuditha Veenath <159011911+VeenathT@users.noreply.github.com>
2024-12-02 10:51:15 +0530

    Add files via upload

---
+++ Tute-2 exercise-3.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+	int mark1,mark2;
+	
+	printf("Enter your mark 1: ");
+	scanf("%d",&mark1);
+	
+	printf("Enter your mark 2: ");
+	scanf("%d",&mark2);
+	
+	printf("Your mark 1 is:%d\nYour mark 2 is:%d",mark1,mark2);
+	
+	return 0;	
+}
